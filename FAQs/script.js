1fbbce4f8d4d66e37a6af2fb3029b6105740fcf2
Abdulrahman Suleiman Boyi <124547020+ABoyiSuleiman38971@users.noreply.github.com>
2025-02-17 04:46:02 +0100

    Add files via upload

---
+++ FAQs/script.js
@@ -0,0 +1,92 @@
+function toggleAnswer() {
+  const iconPlus = document.querySelector(".icon_plus");
+  const iconMinus = document.querySelector(".icon_minus");
+  const statement = document.querySelector(".statement");
+
+
+    // Question 1
+  if (statement.classList.contains('hidden')) {
+      statement.classList.remove('hidden');
+      iconPlus.classList.add('hidden');
+      iconMinus.classList.remove('hidden');
+  } else {
+      statement.classList.add('hidden');
+      iconPlus.classList.remove('hidden');
+      iconMinus.classList.add('hidden');
+  }
+}
+function toggleAnswer2() {
+  const iconPlus2 = document.querySelector(".icon_plus2");
+  const iconMinus2 = document.querySelector(".icon_minus2");
+  const statement2 = document.querySelector(".statement2");
+
+
+    // Question 2
+  if (statement2.classList.contains('hidden')) {
+      statement2.classList.remove('hidden');
+      iconPlus2.classList.add('hidden');
+      iconMinus2.classList.remove('hidden');
+  } else {
+      statement2.classList.add('hidden');
+      iconPlus2.classList.remove('hidden');
+      iconMinus2.classList.add('hidden');
+  }
+}
+function toggleAnswer3() {
+  const iconPlus3 = document.querySelector(".icon_plus3");
+  const iconMinus3 = document.querySelector(".icon_minus3");
+  const statement3 = document.querySelector(".statement3");
+
+
+    // Question 3
+  if (statement3.classList.contains('hidden')) {
+      statement3.classList.remove('hidden');
+      iconPlus3.classList.add('hidden');
+      iconMinus3.classList.remove('hidden');
+  } else {
+      statement3.classList.add('hidden');
+      iconPlus3.classList.remove('hidden');
+      iconMinus3.classList.add('hidden');
+  }
+}
+function toggleAnswer4() {
+  const iconPlus4 = document.querySelector(".icon_plus4");
+  const iconMinus4 = document.querySelector(".icon_minus4");
+  const statement4 = document.querySelector(".statement4");
+
+
+    // Question 3
+  if (statement4.classList.contains('hidden')) {
+      statement4.classList.remove('hidden');
+      iconPlus4.classList.add('hidden');
+      iconMinus4.classList.remove('hidden');
+  } else {
+      statement4.classList.add('hidden');
+      iconPlus4.classList.remove('hidden');
+      iconMinus4.classList.add('hidden');
+  }
+}
+
+    //Question 2
+  // if (statement2.classList.contains('hidden')) {
+  //     statement2.classList.remove('hidden');
+  //     iconPlus.classList.add('hidden');
+  //     iconMinus.classList.remove('hidden');
+  // } else {
+  //     statement2.classList.add('hidden');
+  //     iconPlus.classList.remove('hidden');
+  //     iconMinus.classList.add('hidden');
+  // }
+
+  // Question 3
+//   if (statement3.classList.contains('hidden')) {
+//       statement3.classList.remove('hidden');
+//       iconPlus.classList.add('hidden');
+//       iconMinus.classList.remove('hidden');
+//   } else {
+//       statement3.classList.add('hidden');
+//       iconPlus.classList.remove('hidden');
+//       iconMinus.classList.add('hidden');
+//   }
+// }
+
